8c5fc48107f96c5ef3dd603b7f5a6c4a85a4a686
Niels Slotboom <slotboom.n@gmail.com>
2022-11-16 14:52:55 +0100

    Modified .gitignore

--- Simulation/WDIR.h
+++ Simulation/WDIR.h
@@ -1,3 +1,3 @@
 #pragma once
 #define WDIR "X:\\LC\\LC_sim\\LC_sim\\Simulation"
-#define WDIR "C:\\Users\\Thierry\\Documents\\Uni\\Labcourse\\LC_sim\\Simulation"
+//#define WDIR "C:\\Users\\Thierry\\Documents\\Uni\\Labcourse\\LC_sim\\Simulation"
